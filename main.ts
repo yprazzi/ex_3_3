72c4e433177af215c92832f9607fd27d29813c3c
yprazzi <72910828+yprazzi@users.noreply.github.com>
2020-11-26 09:14:47 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+let born_mini = 10000
+let born_max = 99999
+basic.showString("" + (randint(born_mini, born_max)))
+basic.showString("" + (randint(born_mini, born_max)))
+basic.showString("" + (randint(born_mini, born_max)))
+basic.showString("" + (randint(born_mini, born_max)))
+basic.showString("" + (randint(born_mini, born_max)))
